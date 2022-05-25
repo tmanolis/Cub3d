010161aa06b30c475b36da39b9fc1a09e6386f0f
Marine SANJUAN <marine@sanjuan.eu>
2022-05-25 16:52:10 +0200

    fixed proto of retrieve info in main

--- srcs/main.c
+++ srcs/main.c
@@ -10,7 +10,7 @@ int	main(int argc, char **argv)
 		if (check_arg(argv[1]) == FAILURE)
 			return (write(STDERR_FILENO, "Is not an existing .cub file\n", 30));
 		create_map_from_cub_file(argv[1], &data);
-		if (retrieve_info_in_file(&data, &data.graphics, data.map.file) == FAILURE)
+		if (retrieve_info_in_file(&data, data.map.file) == FAILURE)
 			return (write(2, "Error : does not comply w the rules of the map\n", 63));
 	
 		if (check_info_retrieved(&data.graphics) == FAILURE)
